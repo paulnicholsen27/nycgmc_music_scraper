7f68aae4acb9a377b30ca6b38a7a1e6d7947bca2
Paul Nichols <pnichols104@gmail.com>
2013-02-07 15:05:40 -0500

    Set up to return music page, but not filtering correctly.  Get form_build_id and form_token seem to be working correctly.  Refactored some variable names and whatnot.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 nycgmc_music_scraper
 ====================
+
+Uses requests and beautiful_soup libraries.
+
+This is a scraper built for the New York City Gay Men's Chorus to automate
+the downloading of the sheet music and rehearsal recordings for the current
+concert period.  Feedback greatly encouraged.
--- chorus.py
+++ chorus.py
@@ -1,41 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_form_id(url, cookies):
+def get_form_params(url, cookies):
     '''takes url as input and returns form id and form token (if it exists)'''
+    form_token = None
     page = requests.get(url, cookies=cookies)
     soupped_page = BeautifulSoup(page.content)
-    print soupped_page.content
-    form_id = soupped_page.select('input[name="form_build_id"]')
-    return form_id[0].get('value')
-    #p.text[form_id_index:form_id_index+37]
+    form_build_id = soupped_page.select('input[name="form_build_id"]')
+    form_token = soupped_page.select('input[name="form_token"]')
+    print "form build_id:", form_build_id
+    print "form_token:", form_token
+    if form_token:
+      return form_build_id[0].get('value'), form_token[0].get('value')
+    else:
+      return form_build_id[0].get('value'), None
 
-def login():
-    # p = requests.get('http://nycgmc.groupanizer.com/user/login?destination=/')
-    # form_id_index = p.text.find('id="form-')+4
-    form_id = get_form_id('http://nycgmc.groupanizer.com/user/login?destination=/',{})
-    payload = {'name' : 'paul.nichols', 
-               'pass' : 'Ella27!',
-               'form_build_id' : form_id,
+def login(name='paul.nichols', password='Ella27!'):
+    '''takes as input a username and password, returns logged-in cookie'''
+    form_build_id, form_token = get_form_params('http://nycgmc.groupanizer.com/user/login?destination=/',{})
+    payload = {'name' : name, 
+               'pass' : password,
+               'form_build_id' : form_build_id,
                'form_id' : 'user_login', 
                'op' : "Log in"}
     logged_in = requests.post('http://nycgmc.groupanizer.com/user/login?destination=/', data=payload)
     cookies = logged_in.cookies
     return cookies
 
-cookies = login()
-# music_page = requests.get('http://nycgmc.groupanizer.com/music', cookies = cookies)
-# soupped_page = BeautifulSoup(music_page.content)
-print get_form_id('http://nycgmc.groupanizer.com/music', cookies)
+concert_ids = [85] #will later change to be input by user for diff concerts
 
 def filter_page(*concert_ids):
+    '''takes as input one or more concert ids and returns all music
+    for that concert period'''
+    url = 'http://nycgmc.groupanizer.com/music'
+    form_build_id, form_token = get_form_params(url, cookies)
     for concert in concert_ids:
-        payload = {'terms[]' : concert,
-                   'op' : 'Filter',
-                   'form_build_id' : 'form-7b83873346e4f564c5ed9fc0df651a51',
-                   'form_token' : '34f9f6d6ccb8f302d23f2c5c33b16451',
-                   'form_id' : 'song_list_filters_form'
-                   }
+      print concert
+      payload = {'terms[]' : concert,
+                 'op' : 'Filter',
+                 'form_build_id' : 'form-7b83873346e4f564c5ed9fc0df651a51',
+                 'form_token' : '34f9f6d6ccb8f302d23f2c5c33b16451',
+                 'form_id' : 'song_list_filters_form'
+                 }
+      music_page = requests.get(url, data=payload, cookies=cookies)
+      print music_page.content
 
+# !!! Next step:  Filter concert music.  Currently returning all.  
 
+cookies = login()
+print filter_page(concert_ids)
+#Junk Code
+    #p.text[form_id_index:form_id_index+37]
 
+# p = requests.get('http://nycgmc.groupanizer.com/user/login?destination=/')
+# form_id_index = p.text.find('id="form-')+4
+
+    # music_page = requests.get('http://nycgmc.groupanizer.com/music', cookies = cookies)
