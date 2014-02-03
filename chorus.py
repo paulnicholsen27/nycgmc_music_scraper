6b64b544c47578783c43dff6505b1865355fc319
Paul Nichols <Paul.Nichols@dowjones.com>
2014-02-03 09:33:14 -0500

    refactored, deletes current music folder before downloading new music

--- chorus.py
+++ chorus.py
@@ -1,14 +1,16 @@
 import requests
-import urllib, urllib2
+import urllib
+import urllib2
+import re
+import os
+from os.path import isfile, join
 from bs4 import BeautifulSoup
 
-CONCERT_ID = [130] #will later change to be input by user for diff concerts
-SESSION = requests.session()
 
-def get_form_params(url, cookies):
+def get_form_params(session, url, cookies):
     '''takes url as input and returns form id and form token (if it exists)'''
     form_token = None
-    page = SESSION.get(url, cookies=cookies)
+    page = session.get(url, cookies=cookies)
     soupped_page = BeautifulSoup(page.content)
     form_build_id = soupped_page.select('input[name="form_build_id"]')
     form_token = soupped_page.select('input[name="form_token"]')
@@ -17,9 +19,9 @@ def get_form_params(url, cookies):
     else:
       return form_build_id[0].get('value'), None
 
-def login(name='paul.nichols', password='Ella27!'):
+def login(session, name='paul.nichols', password='Ella27!'):
     '''takes as input a username and password, returns logged-in cookie'''
-    form_build_id, form_token = get_form_params('http://nycgmc.groupanizer.com/user/login?destination=/',{})
+    form_build_id, form_token = get_form_params(session, 'http://nycgmc.groupanizer.com/user/login?destination=/',{})
     print "form build_id:", form_build_id
     print "form_token:", form_token
     payload = {'name' : name, 
@@ -27,25 +29,35 @@ def login(name='paul.nichols', password='Ella27!'):
                'form_build_id' : form_build_id,
                'form_id' : 'user_login', 
                'op' : 'Log in'}
-    logged_in = SESSION.post('http://nycgmc.groupanizer.com/user/login?destination=/', data=payload)
+    logged_in = session.post('http://nycgmc.groupanizer.com/user/login?destination=/', data=payload)
     cookies = logged_in.cookies
     return cookies
 
+def empty_folder(directory):
+  for f in os.listdir(directory):
+    file_path = os.path.join(directory, f)
+    try:
+      if os.path.isfile(file_path):
+        os.unlink(file_path)
+    except Exception, e:
+      print e
 
-def get_music(concert_id):
+def get_music(concert_id, session,directory):
     '''takes as input one concert id and returns all music
     for that concert period'''
     url = 'http://nycgmc.groupanizer.com/music'
-    cookies = login()
-    form_build_id, form_token = get_form_params(url, cookies)
+    cookies = login(session)
+    form_build_id, form_token = get_form_params(session, url, cookies)
     print "concert_id:", concert_id
+    # song_title_pattern = re.compile('(.+)\([v\d]')
+    # song_version_pattern = re.compile('(\d+.\d+)')
     payload = {'terms[]' : concert_id,
                'op' : 'Filter',
                'form_build_id' : form_build_id,
                'form_token' : form_token,
                'form_id' : 'user_login'
                }
-    music_page = SESSION.get(url, data=payload, cookies=cookies)
+    music_page = session.get(url, data=payload, cookies=cookies)
     soup = BeautifulSoup(music_page.content)
     link_list = [link.get('href') for link in soup.find_all('a') if 'node' in link.get('href')] #contains extraneous /node/add link
     try:
@@ -54,19 +66,35 @@ def get_music(concert_id):
       pass
     for link in link_list:
       url = 'http://nycgmc.groupanizer.com' + link
-      song_page = SESSION.get(url)
+      song_page = session.get(url)
       song_page_soup = BeautifulSoup(song_page.content)
 
       music_links = {music_link.text:music_link.get('href') for music_link in song_page_soup.find_all('a') if music_link.get('href').endswith('.pdf')}
       for filename in music_links:
-        if '/' in filename:
-          new_filename = filename.replace('/', '_')
-        else:
-          new_filename = filename
-        f = open('music_files/' + new_filename, 'wb')
-        f.write(SESSION.get(music_links[filename]).content)
+        new_filename = filename.replace('/', '_')
+        print new_filename
+        # song_version = song_version_pattern.search(new_filename)
+        # song_title = song_title_pattern.search(new_filename)
+        # if song_version and song_title:
+        #   song_version = song_version.group(0)
+        #   song_title = song_title.group(1)
+        # else:
+        #   song_title = new_filename
+        # print song_title, song_version
+
+        f = open(directory + new_filename, 'wb')
+        f.write(session.get(music_links[filename]).content)
         f.close()
       
+# def get_files(directory):
+  # existing_files: [ f for f in os.listdir(directory) if isfile(join(directory, f)) and f.endswith('.pdf')]
 
-get_music(CONCERT_ID)
+def main():
+  CONCERT_ID = [130] #will later change to be input by user for diff concerts
+  session = requests.session()
+  directory = '/Users/nicholsp/Dropbox/music_files/'
+  empty_folder(directory)
+  get_music(CONCERT_ID,session, directory)
+
+main()
 print 'finished'
